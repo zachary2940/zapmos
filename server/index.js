a2a5122384536ca8d28fb3347fb64c9434b831ee
Zachary Chua <zchua025@e.ntu.edu.sg>
2019-04-10 16:34:03 +0800

    modified server

--- server/index.js
+++ server/index.js
@@ -5,13 +5,37 @@ const pino = require('express-pino-logger')();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3001);
+console.log('listening on port ', 3001);
+// WARNING: app.listen(80) will NOT work here!
 
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+  res.send(JSON.stringify({ greeting: `Hello ${name}!`, time : time}));
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
-);
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+//res is response
+
+//Routing refers to how an application’s endpoints (URIs) respond to client requests. For an introduction to routing, see Basic routing.
+/*
+req.params contains route parameters (in the path portion of the URL), and req.query contains the URL query parameters (after the ? in the URL).*/ 
+
+//sending data is not really functional yet
+// app.post('/', (req,res)=>{
+
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(JSON.stringify({time: 'hi'}));
+// })
+
--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import openSocket from 'socket.io-client';
+const  socket = openSocket('http://localhost:8000');
 
 class App extends Component {
   constructor(props) {
     super(props);
+    // var test="tiime"
+    // console.log(fetch('/api').then(response => {test=response //this doesnt work because it is not updating
+    //   console.log(response) 
+    //   return response}))
     this.state = {
       name: '',
-      greeting: ''
+      greeting: '',
+      num_mozzies_killed:'',
+      time: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    // this.setTime = this.setTime.bind(this);
   }
+  /**Bind documentation
+   * var module = {
+      x: 42,
+      getX: function() {
+        return this.x;
+      }
+    }
 
-  handleChange(event) {
-    this.setState({ name: event.target.value });
+    var unboundGetX = module.getX;
+    console.log(unboundGetX()); // The function gets invoked at the global scope
+    // expected output: undefined
+
+    var boundGetX = unboundGetX.bind(module);
+    console.log(boundGetX());
+    // expected output: 42
+   */
+
+  handleChange(event) { //a function that changes state
+    this.setState({ name: event.target.value, greeting: "lol" });
   }
 
   handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
+    event.preventDefault();//its default action which is to go to another page is not taken
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)//encode string into URI, fetch returns a Promise that resolves to the Response to that request
+      .then(response => response.json()) //we can get string by taking it from the url but too slow so we use json, parse data using json
+      .then(state => this.setState(state)); //state is the function called if the Promise is fulfilled. setState({time:lol})
   }
+  /*fetch(url)
+  .then(response.something) // Define response type (JSON, Headers, Status codes)
+  .then(data) // get the response type 
+  */
+  // setTime(event) {
+  //   event.preventDefault();
+  //   fetch(`/`)
+  //     .then(response => response.json()) 
+  //     .then(state => this.setState(state)); 
+  // }
+
   render() {
     return (
       <div className="App">
@@ -31,13 +67,14 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name">Enter your name: </label>
+          <p>{this.state.time}</p>
+          <form onSubmit={this.handleSubmit}> {/*if the user clicks on the text within the <label> element, it toggles the control.*/}
+            <label htmlFor="name">Enter your name: </label>  {/*htmlFor, for is to indicate it belongs to id name*/}
             <input
               id="name"
               type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
+              value= {this.state.name} //what is displayed, it will not change if it is a static value
+              onChange={this.handleChange} //input means on type, if this is gone, you cannot type and change
             />
             <button type="submit">Submit</button>
           </form>
